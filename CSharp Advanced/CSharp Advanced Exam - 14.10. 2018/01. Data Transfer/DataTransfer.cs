2de9a593066a880163576816b7b7f3324b578ef4
SimeonVSimeonov <simeonsimeonoff@icloud.com>
2018-10-16 21:45:23 +0300

    first and second exam 14.10.2018 problem

---
+++ CSharp Advanced/CSharp Advanced Exam - 14.10. 2018/01. Data Transfer/DataTransfer.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace _01._Data_Transfer
+{
+    class DataTransfer
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            string input = string.Empty;
+            string pattern = @"^s:([^\;]+);r:([^\;]+);m--(""[a-zA-Z ]+"")";
+            var data = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                var printSender = string.Empty;
+                var printReciver = string.Empty;
+                input = Console.ReadLine();
+                MatchCollection matches = Regex.Matches(input, pattern);
+
+                if (matches.Count == 0)
+                {
+                    continue;
+                }
+                else
+                {
+                    var sender = matches[0].Groups[1].Value;
+                    var reciever = matches[0].Groups[2].Value;
+                    var message = matches[0].Groups[3].Value;
+
+                    foreach (var item in sender)
+                    {
+                        if (char.IsLetter(item))
+                        {
+                            printSender += item;
+                        }
+                        if (char.IsDigit(item))
+                        {
+                            data += int.Parse(item.ToString());
+                        }
+                    }
+                    foreach (var item in reciever)
+                    {
+                        if (char.IsLetter(item))
+                        {
+                            printReciver += item;
+                        }
+                        if (char.IsDigit(item))
+                        {
+                            data += int.Parse(item.ToString());
+                        }
+                    }
+
+                    Console.WriteLine($"{printSender} says {message} to {printReciver}");
+                }
+                Console.WriteLine($"Total data transferred: {data}MB");
+            }
+        }
+    }
+}
---
+++ CSharp Advanced/CSharp Advanced Exam - 14.10. 2018/02. Tagram/Tagram.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Tagram
+{
+    class Tagram
+    {
+        static void Main(string[] args)
+        {
+
+            var tagrams = new Dictionary<string, Dictionary<string, int>>();
+            string input;
+
+            while ((input = Console.ReadLine()) != "end")
+            {
+
+                string[] inputArgs = input.Split(" -> ", StringSplitOptions.RemoveEmptyEntries);
+
+                if (inputArgs[0].Contains("ban"))
+                {
+                    var remove = input.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                    var nameToRemove = remove[1];
+                    if (tagrams.ContainsKey(nameToRemove))
+                    {
+                        tagrams.Remove(nameToRemove);
+                    }
+                }
+
+                else
+                {
+                    var username = inputArgs[0];
+                    var tag = inputArgs[1];
+                    var likes = int.Parse(inputArgs[2]);
+
+                    if (!tagrams.ContainsKey(username))
+                    {
+                        tagrams.Add(username, new Dictionary<string, int>());
+                    }
+                    if (!tagrams[username].ContainsKey(tag))
+                    {
+                        tagrams[username].Add(tag, likes);
+                    }
+
+                   // tagrams.OrderByDescending(x => x.Value);
+                }
+                tagrams = tagrams.OrderByDescending(x => x.Value.Values.Sum()).ToDictionary(x => x.Key, x => x.Value);
+
+            }
+
+            foreach (var item in tagrams)
+            {
+                Console.WriteLine(item.Key);
+                foreach (var kvp in item.Value)
+                {
+                    Console.WriteLine($"- {kvp.Key}: {kvp.Value}");
+
+                }
+            }
+        }
+    }
+}
