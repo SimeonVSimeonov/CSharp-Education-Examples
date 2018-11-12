db673a663df2d32dd9526318a184c7e67037e304
SimeonVSimeonov <simeonsimeonoff@icloud.com>
2018-11-12 22:27:15 +0200

    Inheritance Lab

---
+++ CSharp OOP Basics/InheritanceLab/P05StackOfStrings/StackOfStrings.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace CustomStack
+{
+    public class StackOfStrings
+    {
+        List<string> data;
+
+        public void Push(string item)
+        {
+            this.data.Add(item);
+        }
+
+        public string Pop()
+        {
+            var item = data[data.Count - 1];
+            data.RemoveAt(data.Count - 1);
+            return item;
+        }
+
+        public string Peek()
+        {
+            return data[data.Count - 1];
+        }
+
+        public bool IsEmpty()
+        {
+            return data.Count == 0;
+        }
+    }
+}
