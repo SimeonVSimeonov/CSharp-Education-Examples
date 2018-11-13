12c1fa7589d60fa4d125792be0b9e5981e113aa8
SimeonVSimeonov <simeonsimeonoff@icloud.com>
2018-11-13 11:32:18 +0200

    Interfaces And Abstraction Lab

---
+++ CSharp OOP Basics/InterfacesAndAbstractionLab/P02Cars/Car.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cars
+{
+    public class Car : ICar
+    {
+        public Car(string model, string color)
+        {
+            this.Model = model;
+            this.Color = color;
+        }
+
+        public string Model { get; private set; }
+
+        public string Color { get; private set; }
+
+        public string Start()
+        {
+            return "Engine start";
+        }
+
+        public string Stop()
+        {
+            return "Breaaak!";
+        }
+
+        protected virtual string GetCarInfo()
+        {
+            return $"{this.Color} {this.GetType().Name} {this.Model}";
+        }
+
+        public override string ToString()
+        {
+            var builder = new StringBuilder();
+            builder.AppendLine(this.GetCarInfo())
+                .AppendLine(this.Start())
+                .Append(this.Stop());
+
+            return builder.ToString();
+
+        }
+    }
+}
+
