9fc9bb288d1818f474103c9bb8f80f6188e70ef2
SimeonVSimeonov <simeonsimeonoff@icloud.com>
2019-03-13 19:20:11 +0200

    EntityRelations

---
+++ Databases Advanced Entity Framework/EntityRelations/P01_StudentSystem/Data/StudentSystemContext.cs
@@ -0,0 +1,169 @@
+﻿using Microsoft.EntityFrameworkCore;
+using P01_StudentSystem.Data.Models;
+
+namespace P01_StudentSystem.Data
+{
+    public class StudentSystemContext : DbContext
+    {
+        public StudentSystemContext()
+        {
+
+        }
+
+        public StudentSystemContext(DbContextOptions options) 
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Homework> HomeworkSubmissions { get; set; }
+        public DbSet<StudentCourse> StudentCourses { get; set; }
+        public DbSet<Resource> Resources { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // TODO Configure DB
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(Config.ConnectionString);
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //TODO Constraint and relations
+
+            OnModelCreatingStudent(modelBuilder);
+
+            OnModelCreatingCourse(modelBuilder);
+
+            OnModelCreatingStudentCourse(modelBuilder);
+
+            OnModelCreatingHomework(modelBuilder);
+
+            OnModelCreatingResource(modelBuilder);
+        }
+
+        private static void OnModelCreatingResource(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Resource>(entity =>
+            {
+                entity.HasKey(e => e.ResourceId);
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .IsUnicode();
+
+                entity.Property(e => e.Url)
+                    .IsRequired()
+                    .IsUnicode(false);
+
+                entity.Property(e => e.ResourceType)
+                    .IsRequired();
+
+                entity.HasOne(e => e.Course)
+                    .WithMany(c => c.Resources)
+                    .HasForeignKey(e => e.CourseId)
+                    .OnDelete(DeleteBehavior.Restrict);
+            });
+        }
+
+        private static void OnModelCreatingHomework(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Homework>(entity =>
+            {
+                entity.HasKey(e => e.HomeworkId);
+
+                entity.Property(e => e.Content)
+                    .IsRequired()
+                    .IsUnicode(false);
+
+                entity.Property(e => e.ContentType)
+                    .IsRequired();
+
+                entity.Property(e => e.SubmissionTime)
+                    .IsRequired();
+
+                entity.HasOne(e => e.Student)
+                    .WithMany(s => s.HomeworkSubmissions)
+                    .HasForeignKey(e => e.StudentId)
+                    .OnDelete(DeleteBehavior.Restrict);
+
+                entity.HasOne(e => e.Course)
+                    .WithMany(c => c.HomeworkSubmissions)
+                    .HasForeignKey(e => e.CourseId)
+                    .OnDelete(DeleteBehavior.Restrict);
+            });
+        }
+
+        private static void OnModelCreatingStudentCourse(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<StudentCourse>(entity =>
+            {
+                entity.HasKey(e => new { e.StudentId, e.CourseId });
+
+                entity.HasOne(e => e.Student)
+                    .WithMany(s => s.CourseEnrollments)
+                    .HasForeignKey(e => e.StudentId)
+                    .OnDelete(DeleteBehavior.Restrict);
+
+                entity.HasOne(e => e.Course)
+                    .WithMany(c => c.StudentsEnrolled)
+                    .HasForeignKey(e => e.CourseId)
+                    .OnDelete(DeleteBehavior.Restrict);
+            });
+        }
+
+        private static void OnModelCreatingCourse(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Course>(entity =>
+            {
+                entity.HasKey(e => e.CourseId);
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .IsUnicode()
+                    .HasMaxLength(80);
+
+                entity.Property(e => e.Description)
+                    .IsRequired(false)
+                    .IsUnicode();
+
+                entity.Property(e => e.StartDate)
+                    .IsRequired();
+
+                entity.Property(e => e.EndDate)
+                    .IsRequired();
+
+                entity.Property(e => e.Price)
+                    .IsRequired();
+            });
+        }
+
+        private static void OnModelCreatingStudent(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Student>(entity =>
+            {
+                entity.HasKey(s => s.StudentId);
+
+                entity.Property(s => s.Name)
+                    .IsRequired()
+                    .IsUnicode()
+                    .HasMaxLength(100);
+
+                entity.Property(e => e.PhoneNumber)
+                    .IsRequired(false)
+                    .IsUnicode(false)
+                    .HasColumnType("CHAR(10)");
+
+                entity.Property(e => e.RegisteredOn)
+                    .IsRequired();
+
+                entity.Property(e => e.Birthday)
+                    .IsRequired(false);
+            });
+        }
+    }
+}
