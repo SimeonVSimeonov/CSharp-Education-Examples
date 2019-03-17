9fad7556a5c9ed41c7924a55c8367fdff4922494
SimeonVSimeonov <simeonsimeonoff@icloud.com>
2019-03-17 13:36:23 +0200

    Exam 10 Mar 2019

---
+++ Data Structures/Exam 10 Mar 2019/01. Microsystem - Correctness_Skeleton/Microsystems/Microsystems.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Microsystems : IMicrosystem
+{
+    Dictionary<int, Computer> byNumber = new Dictionary<int, Computer>();
+    Dictionary<string, List<Computer>> byBrand = new Dictionary<string, List<Computer>>();
+
+    public bool Contains(int number)
+    {
+
+        return this.byNumber.ContainsKey(number);
+
+    }
+
+    public int Count()
+    {
+        return this.byNumber.Count();
+    }
+
+    public void CreateComputer(Computer computer)
+    {
+        if (this.byNumber.ContainsKey(computer.Number))
+        {
+            throw new ArgumentException();
+        }
+        this.byNumber.Add(computer.Number, computer);
+
+        string brand = computer.Brand.ToString();
+
+        if (!this.byBrand.ContainsKey(brand))
+        {
+            this.byBrand.Add(brand, new List<Computer>());
+        }
+        this.byBrand[brand].Add(computer);
+    }
+
+    public IEnumerable<Computer> GetAllFromBrand(Brand brand)
+    {
+
+        return this.byNumber.Values.Where(c => c.Brand == brand)
+            .OrderByDescending(c => c.Price);
+    }
+
+    public IEnumerable<Computer> GetAllWithColor(string color)
+    {
+        return this.byNumber.Values.Where(c => c.Color == color)
+            .OrderByDescending(p => p.Price);
+    }
+
+    public IEnumerable<Computer> GetAllWithScreenSize(double screenSize)
+    {
+        return this.byNumber.Values.Where(c => c.ScreenSize == screenSize)
+            .OrderByDescending(n => n.Number);
+    }
+
+    public Computer GetComputer(int number)
+    {
+        if (!this.byNumber.ContainsKey(number))
+        {
+            throw new ArgumentException();
+        }
+
+        return this.byNumber[number];
+    }
+
+    public IEnumerable<Computer> GetInRangePrice(double minPrice, double maxPrice)
+    {
+        return this.byNumber.Values.Where(c => c.Price >= minPrice && c.Price <= maxPrice)
+            .OrderByDescending(p => p.Price);
+    }
+
+    public void Remove(int number)
+    {
+        if (!this.byNumber.ContainsKey(number))
+        {
+            throw new ArgumentException();
+        }
+
+        this.byNumber.Remove(number);
+    }
+
+    public void RemoveWithBrand(Brand brand)
+    {
+        if (!this.byBrand.ContainsKey(brand.ToString()))
+        {
+            throw new ArgumentException();
+        }
+        this.byBrand.Remove(brand.ToString());
+
+    }
+
+    public void UpgradeRam(int ram, int number)
+    {
+        if (!this.byNumber.ContainsKey(number))
+        {
+            throw new ArgumentException();
+        }
+        if (this.byNumber[number].RAM < ram)
+        {
+            this.byNumber[number].RAM = ram;
+        }
+
+    }
+}
---
+++ Data Structures/Exam 10 Mar 2019/02. Vani Planning - Correctness_Skeleton/VaniPlanning/Agency.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Agency : IAgency
+{
+    Dictionary<string, Invoice> bySerialNum = new Dictionary<string, Invoice>();
+
+    public bool Contains(string number)
+    {
+        return this.bySerialNum.ContainsKey(number);
+    }
+
+    public int Count()
+    {
+        return this.bySerialNum.Count;
+    }
+
+    public void Create(Invoice invoice)
+    {
+        if (!this.bySerialNum.ContainsKey(invoice.SerialNumber))
+        {
+            bySerialNum.Add(invoice.SerialNumber, invoice);
+        }
+        else
+        {
+            throw new ArgumentException();
+        }
+    }
+
+    public void ExtendDeadline(DateTime dueDate, int days)
+    {
+        foreach (var item in this.bySerialNum)
+        {
+            if (item.Value.DueDate == dueDate)
+            {
+                item.Value.DueDate.AddDays(days);
+            }
+            else
+            {
+                throw new ArgumentException();
+            }
+        }
+
+    }
+
+    public IEnumerable<Invoice> GetAllByCompany(string company)
+    {
+        return this.bySerialNum.Values.Where(c => c.CompanyName == company)
+            .OrderByDescending(n => n.SerialNumber);
+    }
+
+    public IEnumerable<Invoice> GetAllFromDepartment(Department department)
+    {
+        return this.bySerialNum.Values.Where(d => d.Department == department)
+            .OrderByDescending(t => t.Subtotal)
+            .ThenBy(d => d.IssueDate);
+    }
+
+    public IEnumerable<Invoice> GetAllInvoiceInPeriod(DateTime start, DateTime end)
+    {
+        return this.bySerialNum.Values.Where(d => d.IssueDate >= start && d.IssueDate <= end)
+            .OrderBy(c => c.IssueDate)
+            .ThenBy(d => d.DueDate);
+            
+    }
+
+    public void PayInvoice(DateTime due)
+    {
+        var count = 0;
+        foreach (var item in this.bySerialNum)
+        {
+
+            if (item.Value.DueDate == due)
+            {
+                item.Value.Subtotal = 0.0;
+                count++;
+            }
+        }
+
+        if (count == 0)
+        {
+            throw new ArgumentException();
+        }
+       
+    }
+
+    public IEnumerable<Invoice> SearchBySerialNumber(string serialNumber)
+    {
+
+        return this.bySerialNum.Values.Where(c => c.SerialNumber.Contains(serialNumber))
+            .OrderByDescending(n => n.SerialNumber);
+ 
+     
+    }
+
+    public void ThrowInvoice(string number)
+    {
+        if (!this.bySerialNum.ContainsKey(number))
+        {
+            throw new ArgumentException();
+        }
+        this.bySerialNum.Remove(number);
+    }
+
+    public IEnumerable<Invoice> ThrowInvoiceInPeriod(DateTime start, DateTime end)
+    {
+        var dict = new Dictionary<string, Invoice>();
+
+        foreach (var item in this.bySerialNum)
+        {
+            if (item.Value.DueDate > start && item.Value.DueDate < end)
+            {
+                this.bySerialNum.Remove(item.Key);
+            }
+            else
+            {
+                dict.Add(item.Key, item.Value);
+            }
+        }
+
+        return dict.Values;
+        //throw new NotImplementedException();
+    }
+
+    public void ThrowPayed()
+    {
+        //this.bySerialNum.Values.Where(s => s.Subtotal == 0)
+       
+    }
+}
