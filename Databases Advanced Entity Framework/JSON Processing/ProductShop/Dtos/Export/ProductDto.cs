0660b9db90f49790e2d82b7bc2f46a539c4b1d3d
SimeonVSimeonov <simeonsimeonoff@icloud.com>
2019-03-30 18:26:32 +0200

    JSON Processing

---
+++ Databases Advanced Entity Framework/JSON Processing/ProductShop/Dtos/Export/ProductDto.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProductShop.Dtos.Export
+{
+    public class ProductDto
+    {
+        [JsonProperty(PropertyName = "name")]
+        public string Name { get; set; }
+
+        [JsonProperty(PropertyName = "price")]
+        public decimal Price { get; set; }
+
+        //Seller.FirstName Seller.LastName
+        [JsonProperty(PropertyName = "seller")]
+        public string Seller { get; set; }
+    }
+}
