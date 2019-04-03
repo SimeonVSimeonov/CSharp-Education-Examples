a291d36377ba2fdfeafca3332a9c5f38bee3518a
SimeonVSimeonov <simeonsimeonoff@icloud.com>
2019-04-04 01:17:03 +0300

    fix ConnectionString

--- Databases Advanced Entity Framework/DB Advanced Retake Exam 01.09.2018/VaporStore/Data/Configuration.cs
+++ Databases Advanced Entity Framework/DB Advanced Retake Exam 01.09.2018/VaporStore/Data/Configuration.cs
@@ -3,6 +3,6 @@
 	public static class Configuration
 	{
 		public static string ConnectionString =
-            @"Server=DESKTOP-KDM5RU8\SQLEXPRESS;Database=VaporStore;Trusted_Connection=True";
+            @"Server=.;Database=VaporStore;Trusted_Connection=True";
 	}
 }
