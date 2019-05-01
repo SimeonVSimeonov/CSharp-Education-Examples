1fc660f339cad2070e5a0dfa29fbf206fba44cda
SimeonVSimeonov <simeonsimeonoff@icloud.com>
2019-05-01 20:26:29 +0300

    WEB SERVER EXERCISE

---
+++ CSharp Web Basics/05 WEB SERVER - HTTP PROTOCОL EXERCISE/01.URLDecode/StartUp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Net;
+
+namespace _01.URLDecode
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var url = Console.ReadLine();
+            var decodedUrl = WebUtility.UrlDecode(url);
+            Console.WriteLine(decodedUrl);
+        }
+    }
+}
---
+++ CSharp Web Basics/05 WEB SERVER - HTTP PROTOCОL EXERCISE/02.ValidateURL/StartUp.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Net;
+using System.Text;
+
+namespace _02.ValidateURL
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var urlInput = Console.ReadLine();
+            var decodedUrl = WebUtility.UrlDecode(urlInput);
+
+            try
+            {
+                var parsedUrl = new Uri(decodedUrl);
+                
+                if (string.IsNullOrWhiteSpace(parsedUrl.Scheme) ||
+                    string.IsNullOrWhiteSpace(parsedUrl.Host) ||
+                    string.IsNullOrWhiteSpace(parsedUrl.LocalPath) ||
+                    !parsedUrl.IsDefaultPort)
+                {
+                    throw new ArgumentException("Invalid URL");
+                }
+
+                var sb = new StringBuilder();
+                sb.AppendLine($"Protocol: {parsedUrl.Scheme}")
+                    .AppendLine($"Host: {parsedUrl.Host}")
+                    .AppendLine($"Port: {parsedUrl.Port}")
+                    .AppendLine($"Path: {parsedUrl.LocalPath}");
+
+                if (!string.IsNullOrWhiteSpace(parsedUrl.Query))
+                {
+                    sb.AppendLine($"Query: {parsedUrl.Query.Substring(1)}");
+                }
+
+                if (!string.IsNullOrWhiteSpace(parsedUrl.Fragment))
+                {
+                    sb.AppendLine($"Fragment: {parsedUrl.Fragment.Substring(1)}");
+                }
+                ;
+                Console.WriteLine(sb.ToString().Trim());
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Invalid URL");
+
+            }
+        }
+    }
+}
