ec707d89f89eeb7002757d2650e4ef9caefa5466
SimeonVSimeonov <simeonsimeonoff@icloud.com>
2019-05-09 16:27:12 +0300

    add solution for task 2

--- Algorithms/Problem Solving LAB - Part II/02. Guitar/Program.cs
+++ Algorithms/Problem Solving LAB - Part II/02. Guitar/Program.cs
@@ -1,12 +1,59 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _02._Guitar
 {
     class Program
     {
-        static void Main(string[] args)
+        private static int[] intervals;
+
+        private static int max;
+
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            string[] input = Console.ReadLine().Split(',').ToArray();
+            for (int i = 0; i < input.Length; i++)
+            {
+                input[i] = input[i].Trim();
+            }
+            intervals = input.Select(int.Parse).ToArray();
+            int startDb = int.Parse(Console.ReadLine());
+            max = int.Parse(Console.ReadLine());
+
+            HashSet<int> actual = new HashSet<int>();
+            actual.Add(startDb);
+            HashSet<int> spare = new HashSet<int>();
+
+            for (int i = 0; i < intervals.Length; i++)
+            {
+                foreach (var db in actual)
+                {
+                    int subRes = db - intervals[i];
+                    if (subRes >= 0)
+                    {
+                        spare.Add(subRes);
+                    }
+
+                    int sumRes = db + intervals[i];
+                    if (sumRes <= max)
+                    {
+                        spare.Add(sumRes);
+                    }
+                }
+
+                actual = new HashSet<int>(spare);
+                spare.Clear();
+            }
+
+            if (!actual.Any())
+            {
+                Console.WriteLine("-1");
+            }
+            else
+            {
+                Console.WriteLine(actual.Max());
+            }
         }
     }
 }
