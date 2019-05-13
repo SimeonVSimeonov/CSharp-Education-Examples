5afb2b1757b46d951d5aa101d6b651aa9516ac71
SimeonVSimeonov <simeonsimeonoff@icloud.com>
2019-05-13 10:46:24 +0300

    add shortest path task solution

--- Algorithms/Problem Solving LAB - Part II/03. Shortest Path/Program.cs
+++ Algorithms/Problem Solving LAB - Part II/03. Shortest Path/Program.cs
@@ -1,12 +1,53 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
 namespace _03._Shortest_Path
 {
     class Program
     {
+        public static SortedSet<string> results = new SortedSet<string>();
+
+        public static char[] map;
+
+        public static void Solve(int index)
+        {
+            if (index == map.Length)
+            {
+                results.Add(new string(map));
+                return;
+            }
+            else if (map[index] != '*')
+            {
+                Solve(index + 1);
+            }
+            else
+            {
+                map[index] = 'S';
+                Solve(index + 1);
+                map[index] = 'R';
+                Solve(index + 1);
+                map[index] = 'L';
+                Solve(index + 1);
+                map[index] = '*';
+            }
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string bitsString = Console.ReadLine();
+            map = bitsString.ToCharArray();
+
+            Solve(0);
+
+            var output = new StringBuilder();
+            output.AppendLine(results.Count.ToString());
+            foreach (var result in results)
+            {
+                output.AppendLine(result);
+            }
+
+            Console.WriteLine(output.ToString().Trim());
         }
     }
 }
