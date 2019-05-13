b8d6142e0ec78234bffc596b189a2a7b1334c9b6
SimeonVSimeonov <simeonsimeonoff@icloud.com>
2019-05-13 10:47:34 +0300

    add task 2 parentheses and 3 towns

--- Algorithms/Problem Solving - Part I - Exercise/02. Parentheses/Program.cs
+++ Algorithms/Problem Solving - Part I - Exercise/02. Parentheses/Program.cs
@@ -6,35 +6,46 @@ namespace _02._Parentheses
 {
     class Program
     {
-        static void Main()
+        static readonly StringBuilder result = new StringBuilder();
+        static int n;
+        static int openingCount;
+        static int closingCount;
+        static char[] output;
+
+        static void Main(string[] args)
         {
-           
-            int n = int.Parse(Console.ReadLine());
-            int open = n;
-            int close = n;
-            Stack<char> stack = new Stack<char>();
-            var sb = new StringBuilder();
+            n = int.Parse(Console.ReadLine());
+            output = new char[n * 2];
+            output[0] = '(';
+            openingCount++;
 
-            for (int i = 0; i < n; i++)
+            GenerateParenthesis(1);
+            Console.Write(result);
+        }
+
+        static void GenerateParenthesis(int index)
+        {
+            if (index == output.Length)
             {
-                stack.Push('(');
-                open--;
-                sb.Append('(');
-            }
-            for (int i = 0; i < n; i++)
-            {
-                stack.Push(')');
-                close--;
-                sb.Append(')');
+                result.AppendLine(string.Join("", output));
+                return;
             }
 
-            for (int i = 0; i < n; i++)
+            if (openingCount < n)
             {
-                stack.Pop();
-                close++;
+                output[index] = '(';
+                openingCount++;
+                GenerateParenthesis(index + 1);
+                openingCount--;
             }
 
-            Console.WriteLine(sb.ToString().Trim());
+            if (closingCount < openingCount)
+            {
+                output[index] = ')';
+                closingCount++;
+                GenerateParenthesis(index + 1);
+                closingCount--;
+            }
         }
     }
 }
