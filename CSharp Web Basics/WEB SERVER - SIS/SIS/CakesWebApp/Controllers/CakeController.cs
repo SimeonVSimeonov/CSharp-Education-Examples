7359e8405b3656c8fe39dd666358f54f0e13068b
SimeonVSimeonov <simeonsimeonoff@icloud.com>
2019-05-13 10:52:04 +0300

    add Profile.html and AddCake.html make CakeController.cs

---
+++ CSharp Web Basics/WEB SERVER - SIS/SIS/CakesWebApp/Controllers/CakeController.cs
@@ -0,0 +1,16 @@
+﻿using SIS.HTTP.Requests.Contracts;
+using SIS.HTTP.Responses.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CakesWebApp.Controllers
+{
+    public class CakeController : BaseController
+    {
+        public IHttpResponse AddCakeView(IHttpRequest request)
+        {
+            return this.View("AddCake");
+        }
+    }
+}
