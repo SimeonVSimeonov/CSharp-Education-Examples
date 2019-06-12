12a1f0c6cce22bae4221cffcb2e53b80351027f5
SimeonVSimeonov <simeonsimeonoff@icloud.com>
2019-06-12 16:24:03 +0300

    fix Configuration

--- Databases Advanced Entity Framework/DB Advanced Exam FastFood 10.12.2017/FastFood.Data/Configuration.cs
+++ Databases Advanced Entity Framework/DB Advanced Exam FastFood 10.12.2017/FastFood.Data/Configuration.cs
@@ -2,6 +2,6 @@ namespace FastFood.Data
 {
 	public static class Configuration
 	{
-		public static string ConnectionString = @"Server=DESKTOP-KDM5RU8\SQLEXPRESS;Database=FastFood;Trusted_Connection=True";
+		public static string ConnectionString = @"Server=.\SQLEXPRESS;Database=FastFood;Trusted_Connection=True";
 	}
 }
